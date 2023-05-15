0400566bcc75a14c2010f770715117e00056fcbb
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-05-15 10:10:53 +1000

    commit tree a0f6b81e79f34ad694dd72ba34f300be117714b4

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Code your solution in this file!
+const driversList = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = (driversList) => {
+    return driversList.slice(0,2)
+}
+
+const returnLastTwoDrivers = (driversList) => {
+    return driversList.slice(2,4,)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+selectingDrivers(0)
+selectingDrivers(1)
+
+ function createFareMultiplier(x) {
+    return function(y) {
+       return x * y;
+    };
+ } /* The createFareMultiplier function takes in a single argument, x, and returns a new function. 
+ This returned function takes in a single argument, y, and returns the product of x and y.*/ 
+
+ function fareDoubler(x) {
+    return x * 2;
+ }
+
+ function fareTripler(x) {
+    return x * 3;
+ }
+
+ function selectDifferentDrivers(driversList, functions) {
+    return functions(driversList)
+ }
+
+ selectDifferentDrivers(driversList, returnFirstTwoDrivers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
